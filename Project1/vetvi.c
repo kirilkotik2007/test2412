daba94132e6964465398e123ded3dacfa50b1355
Kirill <kirilkotik2007@gmail.com>
2025-12-24 14:52:13 +0200

    deleted "в списке"

--- Project1/vetvi.c
+++ Project1/vetvi.c
@@ -194,7 +194,7 @@ void main(void) {
     if (b > 0) count++;
     if (c > 0) count++;
 
-    printf("\n Количество положительных чисел в строке = %d\n", count);
+    printf("\n Количество положительных чисел = %d\n", count);
     printf("\n conflict test");
     _getch();
 }
